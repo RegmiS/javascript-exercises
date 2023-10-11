93c30815635258c57d0f109da20feb89eb80c506
RegmiS <sandeshreg35@gmail.com>
2023-10-11 13:32:33 -0500

    Solve jss ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string_input) {
+    let string_rev = "";
+    
+    for ( each_char of string_input){
+        string_rev = each_char + string_rev;
+    }
+
+    return string_rev;
 
 };
 

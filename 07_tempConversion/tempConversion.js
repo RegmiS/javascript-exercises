e0499fa210078435e0ea3c3eede73ba15ecdb15e
RegmiS <sandeshreg35@gmail.com>
2023-10-11 16:26:43 -0500

    Solve js ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input_temp) {
+  let convertedNum = (input_temp - 32) * (5/9);
+  return Math.round(convertedNum * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input_temp) {
+  let convertedNum = (input_temp * (9/5)) + 32;
+  return Math.round(convertedNum * 10) / 10;
 };
 
 // Do not edit below this line

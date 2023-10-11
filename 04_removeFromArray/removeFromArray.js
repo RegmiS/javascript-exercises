670aae075923fac80556fd172aedcb5b9365d8d2
RegmiS <sandeshreg35@gmail.com>
2023-10-11 13:43:51 -0500

    Solve js ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array_input, ...extras) {
+    for (vals in extras){
+        let element_location = array_input.indexOf(vals)
+        if(element_location != -1){
+            array_input.array.splice(location, 1);
+        }
+    }
+
+    return array_input;
 
 };
 

084c21f86345116c4323cde105aa8d8fac743dc1
RegmiS <sandeshreg35@gmail.com>
2023-10-11 13:22:37 -0500

    Solve js ex 02, edit test files to remove skips

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string_repeat, rep_times) {
+    if(rep_times < 0){
+        return 'ERROR';
+    }
+    
+    return string_repeat.repeat(rep_times);
 };
 
 // Do not edit below this line

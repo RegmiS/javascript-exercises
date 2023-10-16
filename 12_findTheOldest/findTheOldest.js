09444544dbce297ee3e0e3edfdcca5e9f2d5055c
RegmiS <sandeshreg35@gmail.com>
2023-10-16 14:05:54 -0500

    Solve js ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return oldest_person = arr.reduce((first, second) => {
+        let firstAge = getAge(first.yearOfBirth, first.yearOfDeath);
+        let secondAge = getAge(second.yearOfBirth, second.yearOfDeath);
+        return firstAge < secondAge ? second: first;
+    });
+};
 
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
